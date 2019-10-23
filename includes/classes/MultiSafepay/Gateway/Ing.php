7dfa3d8e10c3461803144167dedeaebf2976c3af
Sonny <48082757+Sonny-MultiSafepay@users.noreply.github.com>
2019-10-23 09:38:03 +0200

    PLGWOOS-263: Refactor spelling ING Home'Pay

--- includes/classes/MultiSafepay/Gateway/Ing.php
+++ includes/classes/MultiSafepay/Gateway/Ing.php
@@ -30,7 +30,7 @@ class MultiSafepay_Gateway_Ing extends MultiSafepay_Gateway_Abstract
 
     public static function getName()
     {
-        return __('ING HomePay', 'multisafepay');
+        return __("ING Home'Pay", 'multisafepay');
     }
 
     public static function getSettings()
