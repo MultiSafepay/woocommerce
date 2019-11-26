87a262801b42f7651e43af5167fb41bb3cf30b90
Mike De Decker <41482163+Mike-MultiSafepay@users.noreply.github.com>
2019-11-26 16:03:42 +0100

    [CI] Add automated unit testing

---
+++ tests/includes/classes/MultiSafepay/Gateway/test-Ideal.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the MultiSafepay plugin
+ * to newer versions in the future. If you wish to customize the plugin for your
+ * needs please document your changes and make backups before you update.
+ *
+ * @category    MultiSafepay
+ * @package     Connect
+ * @author      MultiSafepay <integration@multisafepay.com>
+ * @copyright   Copyright (c) 2019 MultiSafepay, Inc. (https://www.multisafepay.com)
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+class Multisafepay_Test_Gateway_Ideal extends WC_Unit_Test_Case
+{
+    public function testGetApiKey()
+    {
+        if (!get_option('multisafepay_api_key')) {
+            add_option('multisafepay_api_key');
+        }
+
+        update_option('multisafepay_api_key', '12345');
+
+        $this->assertEquals('12345', MultiSafepay_Gateway_Ideal::getApiKey());
+    }
+}
