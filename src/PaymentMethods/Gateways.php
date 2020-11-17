c7eedb7ba3295bad3633614d7931fba859b51a96
Ronald-MultiSafepay <Ronald.Schonewille@MultiSafepay.com>
2020-11-17 16:50:41 +0100

    PLGWOOS-435: Add Fashiongiftcard
    Signed-off-by: Ronald-MultiSafepay <Ronald.Schonewille@MultiSafepay.com>

--- src/PaymentMethods/Gateways.php
+++ src/PaymentMethods/Gateways.php
@@ -69,6 +69,7 @@ class Gateways {
         'babycadeaubon'  => Babycadeaubon::class,
         'beautywellness' => Beautywellness::class,
         'boekenbon'      => Boekenbon::class,
+        'fashiongiftcard'=> Fashiongiftcard::class,
     );
 
 
