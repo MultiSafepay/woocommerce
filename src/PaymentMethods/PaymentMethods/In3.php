46542b5cbe4ad70e792753cc9eef046c0e4ec31d
Daniel Civit <daniel.civit@multisafepay.com>
2021-02-23 16:30:38 +0100

    PLGWOOS-562: Add fallback for in3, in case no fields is filled in checkout, convert the transaction to redirect type (#250)

--- src/PaymentMethods/PaymentMethods/In3.php
+++ src/PaymentMethods/PaymentMethods/In3.php
@@ -24,6 +24,13 @@
 namespace MultiSafepay\WooCommerce\PaymentMethods\PaymentMethods;
 
 use MultiSafepay\WooCommerce\PaymentMethods\Base\BaseBillingSuitePaymentMethod;
+use MultiSafepay\Api\Transactions\OrderRequest\Arguments\GatewayInfoInterface;
+use MultiSafepay\Api\Transactions\OrderRequest\Arguments\GatewayInfo\Meta as MetaGatewayInfo;
+use MultiSafepay\WooCommerce\PaymentMethods\PaymentMethods\BaseGatewayInfo;
+use MultiSafepay\ValueObject\Customer\EmailAddress;
+use MultiSafepay\ValueObject\Customer\PhoneNumber;
+use MultiSafepay\ValueObject\Date;
+use MultiSafepay\ValueObject\Gender;
 
 class In3 extends BaseBillingSuitePaymentMethod {
 
@@ -97,6 +104,62 @@ class In3 extends BaseBillingSuitePaymentMethod {
      */
     public function get_payment_method_icon(): string {
         return 'in3.png';
+    }
+
+    /**
+     * Validate_fields
+     *
+     * @return  boolean
+     */
+    public function validate_fields(): bool {
+        return true;
+    }
+
+    /**
+     * @param array|null $data
+     * @return MetaGatewayInfo
+     */
+    public function get_gateway_info( array $data = null ): GatewayInfoInterface {
+
+        $gateway_info = new MetaGatewayInfo();
+
+        if ( isset( $_POST[ $this->id . '_salutation' ] ) && '' !== $_POST[ $this->id . '_salutation' ] ) {
+            $gateway_info->addGender( new Gender( $_POST[ $this->id . '_salutation' ] ) );
+        }
+
+        if ( isset( $_POST[ $this->id . '_birthday' ] ) && '' !== $_POST[ $this->id . '_birthday' ] ) {
+            $gateway_info->addBirthday( new Date( $_POST[ $this->id . '_birthday' ] ) );
+        }
+
+        if ( isset( $data ) && ! empty( $data['order_id'] ) ) {
+            $order = wc_get_order( $data['order_id'] );
+            $gateway_info->addEmailAddress( new EmailAddress( $order->get_billing_email() ) );
+            $gateway_info->addPhone( new PhoneNumber( $order->get_billing_phone() ) );
+        }
+
+        if ( ! empty( $gateway_info->getData() ) ) {
+            return $gateway_info;
+        }
+
+        if ( empty( $gateway_info->getData() ) ) {
+            return new BaseGatewayInfo();
+        }
+
+    }
+
+    /**
+     * Check if issuer_id has been set
+     *
+     * @param GatewayInfoInterface $gateway_info
+     * @return boolean
+     */
+    public function validate_gateway_info( GatewayInfoInterface $gateway_info ): bool {
+        $data = $gateway_info->getData();
+        if ( empty( $data['gender'] ) || empty( $data['birthday'] ) ) {
+            $this->type = 'redirect';
+            return false;
+        }
+        return true;
     }
 
 }
