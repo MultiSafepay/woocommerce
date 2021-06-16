1aed487d07a19beacd0436a6e5b2b438118668d2
Daniel Civit <daniel.civit@multisafepay.com>
2021-06-16 15:48:02 +0200

    PLGWOOS-706: Declare support for WooCommerce 5.4.1 (#358)

--- multisafepay.php
+++ multisafepay.php
@@ -33,7 +33,7 @@
  * Requires at least:       5.0
  * Tested up to:            5.7.2
  * WC requires at least:    4.2.0
- * WC tested up to:         5.3.0
+ * WC tested up to:         5.4.1
  * Requires PHP:            7.2
  * Text Domain:             multisafepay
  * Domain Path:             /languages
