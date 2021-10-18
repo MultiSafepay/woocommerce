83c3013a2f75bd0189bc4a21c2e64671bd342d85
Daniel Civit <daniel.civit@multisafepay.com>
2021-10-18 15:45:05 +0200

    PLGWOOS-746: Declare support for latest WooCommerce and Wordpress versions (#384)

--- multisafepay.php
+++ multisafepay.php
@@ -31,9 +31,9 @@
  * License:                 GNU General Public License v3.0
  * License URI:             http://www.gnu.org/licenses/gpl-3.0.html
  * Requires at least:       5.0
- * Tested up to:            5.8.0
+ * Tested up to:            5.8.1
  * WC requires at least:    4.2.0
- * WC tested up to:         5.6.0
+ * WC tested up to:         5.8.0
  * Requires PHP:            7.2
  * Text Domain:             multisafepay
  * Domain Path:             /languages
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: multisafepayplugin
 Tags: multisafepay, credit card, credit cards, gateway, payments, woocommerce, ideal, bancontact, klarna, sofort, giropay, sepa direct debit
 Requires at least: 5.0
-Tested up to: 5.8.0
+Tested up to: 5.8.1
 Requires PHP: 7.2
 Stable tag: 4.8.3
 License: MIT
